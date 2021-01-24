cbc4188221008a7061d64f60e32345b3edc73b29
blaueQuelle <jk@blauequelle.de>
2021-01-24 11:16:01 +0100

    updated GIT Operator

--- src/Api/Operator/Git.php
+++ src/Api/Operator/Git.php
@@ -3,13 +3,33 @@
 
 namespace PleskX\Api\Operator;
 
+use PleskX\Api\Client\Exception;
+use PleskX\Api\Operator;
+use PleskX\Api\XmlResponse;
 
-class Git extends \PleskX\Api\Operator
+class Git extends Operator
 {
-
-    public function create()
+    /**
+     * @param string $domain
+     * @param string $name
+     * @param string $deploymentMode
+     * @param string $deploymentPath
+     * @return XmlResponse
+     * @throws Exception|\PleskX\Api\Exception
+     */
+    public function create(string $domain, string $name, string $deploymentMode = "auto", string $deploymentPath = "httpdocs"): XmlResponse
     {
         $packet = $this->_client->getPacket();
-        rd($this);
+        $info = $packet->addChild("extension")->addChild("call")->addChild("git")->addChild("create");
+        $info->addChild("domain", $domain);
+        $info->addChild("name", $name);
+        $info->addChild("deployment-mode", $deploymentMode);
+        $info->addChild("deployment-path", $deploymentPath);
+
+        $response = $this->_client->request($packet);
+
+        ray($this, $packet, $info, $response);
+
+        return $response;
     }
 }
